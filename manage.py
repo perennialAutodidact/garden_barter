1621fd455b408dc7b06a621e597f78d403875a10
Keegan Good <keegood8@gmail.com>
2022-04-16 20:19:27 -0700

    changed project name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seed_swap_proj.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'garden_barter_proj.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
