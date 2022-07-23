b757773d23f9fa658faffb1b10aa710a5bbdc88e
Keegan Good <keegood8@gmail.com>
2022-07-23 10:01:34 -0700

    added serialized user fields

--- messages_app/serializers.py
+++ messages_app/serializers.py
@@ -1,22 +1,27 @@
 from rest_framework import serializers
 from .models import Inbox, Message, Conversation
-
+from users_app.serializers import UserMessageSerializer
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = UserMessageSerializer()
+    recipient = UserMessageSerializer()
+    
     class Meta:
         model = Message
         fields = '__all__'
 
 class ConversationSerializer(serializers.ModelSerializer):
     messages = MessageSerializer(many=True)
+    sender = UserMessageSerializer()
+    recipient = UserMessageSerializer()
 
     class Meta:
         model = Conversation
-        fields = '__all__'
+        fields = ['id', 'messages', 'sender', 'recipient']
 
 class InboxSerializer(serializers.ModelSerializer):
     conversations = ConversationSerializer(many=True)
-
+    user = UserMessageSerializer()
     class Meta:
         model = Inbox
         fields = '__all__'
