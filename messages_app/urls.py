ce76c0e3fc320f05e336e115d47a1572cf6b5541
Keegan Good <keegood8@gmail.com>
2022-07-24 13:24:33 -0700

    added url pattern for convos/find/

--- messages_app/urls.py
+++ messages_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns=[
     path('', views.inbox, name='inbox'),
     path('messages/create/', views.create, name="create"),
     # path('messages/<int:message_id>/', views.inbox, name='message_detail'),
+    path('conversations/find/', views.find_conversation, name="find_conversation"),
     path('conversations/<int:conversation_id>/', views.conversations, name='conversations')
 ]
