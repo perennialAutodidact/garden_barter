bbf2496676377dd56916538d91ed02a97da96eb7
Keegan Good <keegood8@gmail.com>
2022-07-24 13:22:34 -0700

    changed 1-1 field to fk

--- messages_app/models.py
+++ messages_app/models.py
@@ -13,17 +13,17 @@ class Conversation(models.Model):
     inbox = models.ForeignKey(Inbox, on_delete=models.CASCADE, related_name='conversations')
     barter_id = models.PositiveIntegerField(verbose_name=_('barter id'))
     barter_type = models.CharField(_('barter type'), max_length=10)
-    sender = models.OneToOneField(get_user_model(),
+    sender = models.ForeignKey(get_user_model(),
         on_delete=models.CASCADE, verbose_name=_('sender'), related_name="coversations")
-    recipient = models.OneToOneField(get_user_model(),
+    recipient = models.ForeignKey(get_user_model(),
         on_delete=models.CASCADE, verbose_name=_('recipient'), related_name="conversations")
 
 class Message(models.Model):
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, verbose_name=_(
         'conversation'), related_name='messages')
-    sender = models.OneToOneField(get_user_model(), 
+    sender = models.ForeignKey(get_user_model(), 
         on_delete=models.CASCADE, verbose_name=_('sender'), related_name="messages_sent")
-    recipient = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, verbose_name=_(
+    recipient = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_(
         'recipient'),  related_name="messages_received")
     date_received = models.DateTimeField(_('date received'), auto_now_add=True)
     body = models.TextField(_('body'), max_length=1000)
