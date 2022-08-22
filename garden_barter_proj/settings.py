8c48b9402c2e1f52bfaae28feb76afda127a17eb
KG <keegood8@gmail.com>
2022-08-22 14:16:25 -0700

    shorted access token lifespan for testing frontend auth flow

--- garden_barter_proj/settings.py
+++ garden_barter_proj/settings.py
@@ -151,7 +151,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
